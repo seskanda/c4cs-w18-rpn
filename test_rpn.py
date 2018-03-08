a155a6785726cea99b6fe6df23daee925b31b676
Sara E Eskandari <seskanda@caen-vnc-vm12.engin.umich.edu>
2018-03-07 21:13:45 -0500

    added test for rpn eponentiation

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,6 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
+	def test_exponentiation(self):
+		result = rpn.calculate('3 3 ^')
+		self.assertEqual(9, result)
